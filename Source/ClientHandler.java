94fcb7486f5fdf3869a4246fe39124ca5ed6b37f
Zachary Londono <zcl6@scarletmail.rutgers.edu>
2020-09-17 22:04:54 -0400

    Timeout after 5000ms

--- Source/ClientHandler.java
+++ Source/ClientHandler.java
@@ -1,7 +1,6 @@
 import java.net.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 public class ClientHandler implements Runnable {
 
@@ -20,6 +19,7 @@ public class ClientHandler implements Runnable {
                 DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());) {
 
             String response = "";
+	    
             boolean timedOut = false;
             long connectedTime = System.currentTimeMillis();
             while (!reader.ready()) {
@@ -27,36 +27,42 @@ public class ClientHandler implements Runnable {
                     // Client has timed out
                     response = supportedVerison + " " + StatusCode._408.toString();
                     timedOut = true;
+		    break;
                 }
             }
 
             if (!timedOut) {
+
+	        // Read in all lines from client
                 String line;
-                ArrayList<String> lines = new ArrayList<>();
-                while ((line = reader.readLine()) != null) {
-                    lines.add(line);
-                }
+                ArrayList<String> lines = new ArrayList<>();		
+                while (reader.ready() && (line = reader.readLine()) != null) {
+			lines.add(line);
+			System.out.println(lines.get(lines.size() - 1));
+		}
 
-                String[] request = (String[]) lines.toArray();
-                String[] fields = request[0].split(Character.toString(32));
-
+                String[] request = lines.toArray(String[]::new); 
+                String[] fields = request[0].split(Character.toString(32));	// split the first line into fields to validate request
+		
+		// Check that request is valid
                 if (fields.length != 3) response = StatusCode._400.toString();
                 else if (!fields[2].equals(supportedVerison)) response = supportedVerison + " " + StatusCode._505.toString();
                 else if (!handlerMap.containsKey(fields[0])) response = supportedVerison + " " + StatusCode._400.toString();
-                else response = handlerMap.get(fields[0]).handler(request);    
-            }
+                else response = handlerMap.get(fields[0]).handler(request);    // Generate response 
+           
+	   }
 
             System.out.println("Sending: " + response);
-            output.writeBytes(response);
+            output.writeBytes(response);	// Send response back to client
             output.flush();
 
             try {
-                Thread.sleep(250);
+                Thread.sleep(250);	// Wait 1/4 second
             } catch(InterruptedException e) {
                 System.err.println("[Error] error occured while sending message to client");
             }
 
-            
+            // Close socket I/O
             reader.close();
             output.close();
 
