8d3bb23f9efe3842a3c1a9e1b2ba3c881ca98d55
Zachary Londono <zcl6@scarletmail.rutgers.edu>
2020-10-01 19:46:09 -0400

    added comments, fixed some formatting

--- Source/PartialHTTP1Server.java
+++ Source/PartialHTTP1Server.java
@@ -36,12 +36,12 @@ public class PartialHTTP1Server {
             
             System.out.println("Server started on port: " + PORT);
 	
-	    // Maps a given function name to it's defined method
+            // Maps a given function name to it's defined method
             handlerMap = new HashMap<>();
             handlerMap.put("GET", PartialHTTP1Server::GET);
             handlerMap.put("POST", PartialHTTP1Server::POST);
             handlerMap.put("HEAD", PartialHTTP1Server::HEAD);
-	    // The following functions are not implimented
+            // The following functions are not implimented
             handlerMap.put("PUT", (request) ->  ("HTTP/1.0 " + StatusCode._501.toString()).getBytes());
             handlerMap.put("DELETE", (request) ->("HTTP/1.0 " + StatusCode._501.toString()).getBytes());
             handlerMap.put("LINK", (request) ->  ("HTTP/1.0 " + StatusCode._501.toString()).getBytes());
@@ -51,9 +51,9 @@ public class PartialHTTP1Server {
 
             Socket clientSocket;
             while ((clientSocket = serverSocket.accept()) != null) {
-	        // Accepts a new connection from a client and submits it to a thread executor to be handled by the thread handler
+                // Accepts a new connection from a client and submits it to a thread executor to be handled by the thread handler
                 System.out.println("New Connection From: " + clientSocket.getInetAddress());
-		setActiveCount(getActiveCount() + 1);
+                setActiveCount(getActiveCount() + 1);
                 executor.submit(new ClientHandler(clientSocket, handlerMap));
             }
 
@@ -114,36 +114,30 @@ public class PartialHTTP1Server {
     }
 
     // Returns the content of a given file as an array of the file's bytes, if its unable to read the file for any reason it will return null
-    private static byte[] getFileContent(File file) {
-        try (FileInputStream reader = new FileInputStream(file)) {
-            int offset = 0;
-            byte[] buf = new byte[1024];
-            int readin;
-            while ((readin = reader.read(buf, offset, buf.length - offset)) != -1) {
-                offset += readin;
-                if (buf.length == offset) buf = Arrays.copyOf(buf, buf.length * 2);
-            }
-            return Arrays.copyOfRange(buf,0, offset);
-        } catch(Exception e) {
-
-            e.printStackTrace();
-            
-            return null;
+    private static byte[] getFileContent(File file) throws FileNotFoundException, IOException {
+        FileInputStream reader = new FileInputStream(file);
+        int offset = 0;
+        byte[] buf = new byte[1024];
+        int readin;
+        while ((readin = reader.read(buf, offset, buf.length - offset)) != -1) {
+            offset += readin;
+            if (buf.length == offset) buf = Arrays.copyOf(buf, buf.length * 2);
         }
+        reader.close();
+        return Arrays.copyOfRange(buf,0, offset);
     }
 
     private static String getHeaders(File file) throws FileNotFoundException, AccessDeniedException {
 
+        // Throw exception if we can't create the headers
         if(!file.exists()) throw new FileNotFoundException();
-
         if(!file.canRead()) throw new AccessDeniedException("Couldn't read file");
         
-        String headers = "Content-Type: " + getMimeType(file.getName()) + CRLF;
-        headers += "Content-Length: " + file.length() + CRLF;
-
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss z");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
+        String headers = "Content-Type: " + getMimeType(file.getName()) + CRLF;
+        headers += "Content-Length: " + file.length() + CRLF;
         headers += "Last-Modified: " + sdf.format(new Date(file.lastModified())) + CRLF;
         headers += "Expires: Tue, 1 Jan 2021 1:00:00 GMT" + CRLF;
         headers += "Allow: GET, POST, HEAD" + CRLF;
@@ -169,58 +163,48 @@ public class PartialHTTP1Server {
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss z");
         sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
-        Date date1 = null;
-        int truth = 0;
-        //we have to split up the lines. We really only need two lines. First we have to check if request.length is greater than 1. If its greater than 1 then we take the first and check the rest. The moment we reach something that matches If-Modified-Since: then we can save that and stop the for loop looking for it.
-        if (request.length > 1){
-
-            //we must extract the If modified since value.
-            String test = "If-Modified-Since:";
-            String lastmodified = "";
-            int x = 0;
-            for(x=0;x<request.length;x++){
-                if(request[x].contains(test)) {
-                    lastmodified = request[x].substring(request[x].indexOf(" ") + 1);
-                    try{
-                        // If the if-modified-since date is of an invalid format, a ParseException will be thrown, so we can ignore the if-modified-date
-                        date1 = sdf.parse(lastmodified);
-                        truth = 1;
-                    } catch(ParseException ex){
-                        truth = 0;
-                    }
-                    break;
-                }
-            }
+        try {
             
-        }
-
-        try{
-
             String cwd = new java.io.File(".").getCanonicalPath();
             File file = new File(cwd + "/"+ resource);
-        
+
             // Generate headers for response
             response += getHeaders(file);
 
-            Date checkIfGreater1 = new Date(file.lastModified());
+            // If the request has headers, we need to check if it has the If-Modified-Since header
+            if (request.length > 1){
+                int x = 0;
+                for(x=0;x<request.length;x++){
+                    if(request[x].contains("If-Modified-Since:")) {
+                        // Get the date substring from the header
+                        String ifModifiedString = request[x].substring(request[x].indexOf(" ") + 1);
+                        try{
+                            // If the if-modified-since date is of an invalid format, a ParseException will be thrown, so we can ignore the if-modified-date
+                            Date ifModifiedDate = sdf.parse(ifModifiedString);
+                            Date lastModifiedDate = new Date(file.lastModified());
+                            // Compare it against file's last modified date, if the file is older than the ifModifiedDate, then we return status 304 Not Modified, with the expiration date 
+                            if (lastModifiedDate.compareTo(ifModifiedDate) < 0)
+                                response = "HTTP/1.0 " + StatusCode._304.toString() + CRLF +  "Expires: Tue, 1 Jan 2021 1:00:00 GMT" + CRLF;;
+                            break;
+                        } catch(ParseException ex){
+                            // malformed if-modified-since, ignore it
+                            break;                            
+                        }
+                    }
+                }
 
-            // if truth == 1 that means that the request contains a valid if-modified-since header 
-            if (truth == 1 && checkIfGreater1.compareTo(date1)<0){
-                response = "HTTP/1.0 " + StatusCode._304.toString() + CRLF +  "Expires: Tue, 1 Jan 2021 1:00:00 GMT" + CRLF;;
-            } else { // if truth == 0 || (truth == 1 && checkIfGreater1.compareTo(date1) >= 0) 
+            } 
 
-                byte[] payload = getFileContent(file);
-                response +=  CRLF + CRLF;
-                byte[] response_bytes = response.getBytes();
-                byte[] message = new byte[response_bytes.length + payload.length];
+            byte[] payload = getFileContent(file);
+            response +=  CRLF + CRLF;
+            byte[] response_bytes = response.getBytes();
+            byte[] message = new byte[response_bytes.length + payload.length];
 
-                // combine payload and response
-                System.arraycopy(response_bytes, 0, message, 0, response_bytes.length);
-                System.arraycopy(payload, 0, message, response_bytes.length, payload.length);
-            
-                return message;
-
-            }
+            // combine payload and response
+            System.arraycopy(response_bytes, 0, message, 0, response_bytes.length);
+            System.arraycopy(payload, 0, message, response_bytes.length, payload.length);
+        
+            return message;
 
         }catch (AccessDeniedException e) {
             response = "HTTP/1.0 " + StatusCode._403.toString();
